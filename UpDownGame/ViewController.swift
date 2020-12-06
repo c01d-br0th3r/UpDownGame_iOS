ef630ab8fabecaaf90075951ac400e5e31ee5be3
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-12-07 01:55:24 +0900

    Finished ~Lesson 3

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.slider.value = 15
+        slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
     }
 
 
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        print("touch up reset button")
+    }
 }
 
